802811dd01ffc0a45ec3234f16ad60062d846439
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-02-25 09:26:36 -0300

    primeiro programa em java

---
+++ Aula01/programa1/olamundo.java
@@ -0,0 +1,10 @@
+class olamundo {
+    public static void main(String[] args){
+        System.out.println("Ola mundo!");
+        System.out.println("Stein e mal?");
+        /*
+        comentáriario
+        */
+
+    }
+}
