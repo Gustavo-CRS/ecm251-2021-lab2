1c918a4d962802d79cc4b07dd4da050ed442d10d
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-03-01 14:04:21 -0300

    att e criacao aula03

---
+++ Aula03/src/souza/ramirez/consoleti/gustavo/Aula03.java
@@ -0,0 +1,7 @@
+package souza.ramirez.consoleti.gustavo;
+
+public class Aula03 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
