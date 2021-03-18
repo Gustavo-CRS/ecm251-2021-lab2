2b941ce14cc16af595fdbe57f43b7a4c763709f8
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-03-18 09:21:41 -0300

    criação validador de cpf

---
+++ Aula05-teoria/validadorcpf/src/souza/ramires/consoleti/gustavo/Main.java
@@ -0,0 +1,9 @@
+package souza.ramires.consoleti.gustavo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "12345678912";
+        System.out.println("Cpf valido:" + validadorCPF.validarCpf(cpf));
+    }
+}
--- Aula06/src/souza/ramirez/consoleti/gustavo/Main.java
+++
@@ -1,9 +0,0 @@
-package souza.ramirez.consoleti.gustavo;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Sistema sistema = new Sistema();
-        sistema.executar();
-    }
-
-}
