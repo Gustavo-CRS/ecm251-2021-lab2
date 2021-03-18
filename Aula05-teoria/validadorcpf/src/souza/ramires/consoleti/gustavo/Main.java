67a1e0a46132c47b3d81888402e4f844417dfe00
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-03-18 09:33:58 -0300

    Update Main.java

--- Aula05-teoria/validadorcpf/src/souza/ramires/consoleti/gustavo/Main.java
+++ Aula05-teoria/validadorcpf/src/souza/ramires/consoleti/gustavo/Main.java
@@ -3,7 +3,7 @@ package souza.ramires.consoleti.gustavo;
 public class Main {
 
     public static void main(String[] args) {
-        String cpf = "12345678912";
+        String cpf = "12345678910";
         System.out.println("Cpf valido:" + validadorCPF.validarCpf(cpf));
     }
 }
