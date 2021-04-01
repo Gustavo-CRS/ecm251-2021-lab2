c38cc45f41f098667bc41ebe58f9b9da4bef722f
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-04-01 19:26:59 -0300

    formatação do código

--- T1/T1 PROG/src/Usuario.java
+++ T1/T1 PROG/src/Usuario.java
@@ -1,13 +1,10 @@
-  
+
 /**
  * 
- *  @author Guilherme Samuel de Souza Barbosa 19.00012-0 <gui.samuel10@gmail.com>
- *  @author Gustavo Consoleti Ramirez de Souza 19.00715-9 <gugacon@hotmail.com>
+ * @author Guilherme Samuel de Souza Barbosa 19.00012-0 <gui.samuel10@gmail.com>
+ * @author Gustavo Consoleti Ramirez de Souza 19.00715-9 <gugacon@hotmail.com>
  * 
  */
-
-
-
 
 public class Usuario {
     // Atributos
@@ -15,7 +12,6 @@ public class Usuario {
     private String senha;
     private String email;
     private Conta conta;
-    
 
     // Construtor
     public Usuario(String nome, String senha, String email) {
@@ -23,7 +19,6 @@ public class Usuario {
         this.senha = senha;
         this.email = email;
     }
-    
 
     // Getters
     public String getNome() {
@@ -42,18 +37,17 @@ public class Usuario {
         return this.conta;
     }
 
-    public String gerarRequisicao(double valor) { //Retorna: 1;All Might;250.0;9665
+    public String gerarRequisicao(double valor) { // Retorna: 1;All Might;250.0;9665
         this.conta.gerarQRCode(this.nome, valor);
         String qrCodeUser = Transacoes.pegarQRcode(this.conta.getIdConta(), this.nome, valor);
-        return qrCodeUser;       
+        return qrCodeUser;
     }
-
 
     public void pagarUsuario(Usuario user) {
         String[] dados = user.conta.getQrCode().split(";");
         double valorDaConta = Double.parseDouble(dados[2]);
 
-        if (Transacoes.pagarRequisicao(this, user, valorDaConta)){
+        if (Transacoes.pagarRequisicao(this, user, valorDaConta)) {
             System.out.println("Transação realizada com sucesso!");
 
         } else {
