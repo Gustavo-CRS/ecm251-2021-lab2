c1a06076ef05994f08134b5c7ba16687a415a057
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-06-07 12:03:56 -0300

    refatoração na classe da interface do repositório e do repositório
    Co-Authored-By: Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>

--- T2/T2/src/consoleti/gustavo/t2/repositories/IRepository.java
+++ T2/T2/src/consoleti/gustavo/t2/repositories/IRepository.java
@@ -1,11 +1,13 @@
 package consoleti.gustavo.t2.repositories;
 
+import java.util.ArrayList;
+
 import consoleti.gustavo.t2.models.Membros;
 
 public interface IRepository {
-    Membros adicionarMembro(Membros membro);
-    Membros removeMembros(Membros membro);
-    Membros getMembros(Membros membro);
+    void adicionarMembro(Membros membro);
+    void removerMembros(Membros membro);
+    ArrayList<Membros> getMembros(Membros membro);
 
 
 }
--- T2/T2/src/consoleti/gustavo/t2/repositories/Repository.java
+++ T2/T2/src/consoleti/gustavo/t2/repositories/Repository.java
@@ -4,28 +4,8 @@ import java.util.ArrayList;
 
 import consoleti.gustavo.t2.models.Membros;
 
-public class Repository {
+public class Repository implements IRepository {
     private ArrayList<Membros> listaMembros = new ArrayList<Membros>();
-
-    /**
-     * método para adicionar um novo membro.
-     * 
-     * @param membro
-     */
-
-    public void adicionarMembro(Membros membro) {
-        this.listaMembros.add(membro);
-
-    }
-
-    /**
-     * método para remover um membro
-     * 
-     * @param membro
-     */
-    public void removerMembro(Membros membro) {
-        this.listaMembros.remove(membro);
-    }
 
     /**
      * método para pegar a lista de membros.
@@ -33,6 +13,39 @@ public class Repository {
      * @return a lista de membros
      */
     public ArrayList<Membros> getListaMembros() {
+        return listaMembros;
+    }
+
+    /**
+     * método para adicionar novos membros.
+     * 
+     * @param membro
+     */
+    @Override
+    public void adicionarMembro(Membros membro) {
+        this.listaMembros.add(membro);
+
+    }
+
+    /**
+     * método para remover membros.
+     * 
+     * @param membro
+     */
+    @Override
+    public void removerMembros(Membros membro) {
+        this.listaMembros.remove(membro);
+
+    }
+
+    /**
+     * método para pegar a lista de membros.
+     * 
+     * @return a lista de membros
+     */
+    @Override
+    public ArrayList<Membros> getMembros(Membros membro) {
+
         return listaMembros;
     }
 
