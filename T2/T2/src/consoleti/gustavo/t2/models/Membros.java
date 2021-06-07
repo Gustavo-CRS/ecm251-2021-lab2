e37ba6fd03e412daefd0984ce8ce9146e93a3cc3
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-06-07 10:45:40 -0300

    criação da classe membros e seus métodos e atributos

---
+++ T2/T2/src/consoleti/gustavo/t2/models/Membros.java
@@ -0,0 +1,88 @@
+package consoleti.gustavo.t2.models;
+
+import consoleti.gustavo.t2.enums.Funcoes;
+
+public abstract class  Membros {
+    /**
+     * Atributos da classe
+     */
+    private String email;
+    private String nome;
+    private Funcoes funcao;
+
+    /**
+     * Construtor dos atributos da classe Membro
+     * @param email
+     * @param nome
+     * @param funcao
+     */
+    public Membros(String email, String nome, Funcoes funcao) {
+        this.email = email;
+        this.nome = nome;
+        this.funcao = funcao;
+    }
+
+    /**
+     * Getter do email do membro
+     * @return String
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Setter do email do membro
+     * @param email
+     * @return String
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Getter do nome do membro
+     * @return String
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Setter do nome do membro
+     * @param nome
+     * @return String
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Getter do funcao do membro
+     * @return Funcoes
+     */
+    public Funcoes getFuncao() {
+        return funcao;
+    }
+
+    /**
+     * Setter da funcao do membro
+     * @param funcao
+     * @return Funcoes
+     */
+    public void setFuncao(Funcoes funcao) {
+        this.funcao = funcao;
+    }
+
+    /**
+     * Printa os atributos do membro
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "Membro [email=" + email + ", funcao=" + funcao + ", nome=" + nome + "]";
+    }
+    
+
+
+    
+}
