b0b224c0cf709b1816fb7e6944abf708edaab6c7
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-06-07 12:09:30 -0300

    mudança do run na classe App
    Co-Authored-By: Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>

--- T2/T2/src/consoleti/gustavo/t2/App.java
+++ T2/T2/src/consoleti/gustavo/t2/App.java
@@ -1,10 +1,18 @@
+/**
+ * @author Guilherme Samuel de Souza Barbosa 19.00012-0
+ * @author Gustavo Consoleti Ramirez de Souza 19.00715-9
+ * 
+ */
+
+
 package consoleti.gustavo.t2;
 
 import consoleti.gustavo.t2.controller.Sistema;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-       Sistema sistema = new Sistema();
-       sistema.run();
+       Sistema.run();
     }
 }
